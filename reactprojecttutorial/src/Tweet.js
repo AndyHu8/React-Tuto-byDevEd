01aa39a931fddcdb6dd3ca3d68b2ce4b3bbb8279
Andy Hu <huandy01@gmail.com>
2021-07-21 16:22:14 +0200

    Los gehts

---
+++ reactprojecttutorial/src/Tweet.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Tweet(){
+    return(
+        //https://www.youtube.com/watch?v=dGcsHMXbSOA&list=PLDyQo7g0_nsVHmyZZpVJyFn5ojlboVEhE (21:26)
+        <div></div>
+    );
+}
